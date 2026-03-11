5339d60be1d5e9014b818d4eafb434e221d2b429
Samuel Castillo <samuelcastillo0508@gmail.com>
2026-03-11 09:53:07 -0500

    Primer main con instrucciones claras

---
+++ src/Main/Main.java
@@ -0,0 +1,45 @@
+package Main;
+
+public class Main {
+    static void main(String[] args) {
+        /*
+        Chicos aqui va ir todo el main, ya cada uno va a crear su propia branch y despues las juntamos
+        Lo que tienen que hacer es ir editando en su branch y cuando ya tengan un metodo de la clase lista
+        o la clase lista hacen un pull request y ya lo revisamos y lo subimos al main, suban cosas cada que puedan
+        si no saben como subir cosas este es el proceso:
+
+
+        TODO EN LA TERMINAL DE INTELLIJ
+
+        HAGAN ESTO SOLO LA PRIMERA VEZ QUE SE VAN A CONECTAR AL GITHUB DESDE INTELL
+
+        1. CLONAR EL REPOSITORIO (solo la primera vez)
+        git clone https://github.com/Samuel-Castillo0910/Taller-POO.git
+
+
+        Primero descargan lo que es en el main para ir al dia y no dañar la principal
+        1. Actualizar proyecto antes de empezar:
+        git checkout main (se va a la rama principal osea el main, si quieren ver en que branch estan es git branch)
+        git pull origin main (accede a el codigo que esta en el main
+
+        2. Crear propia branch para trabajar:
+        git checkout -b nombre-branch
+
+        3. Programar normalmente.
+
+        4. Subir cambios:
+        git add . (si solo quieren subir un archivo es git add *****.java)
+        git commit -m "mensaje explicando cambios" (Expliquen que cambiaron y haganlo bien carechimbas)
+        git push -u origin nombre-branch
+
+        5. Crear Pull Request en GitHub:
+        nombre-branch -> main
+
+        REGLAS:
+        - No trabajar directamente en main.
+        - Cada persona usa su propia branch.
+        - Hacer pull antes de empezar a programar.
+        */
+    }
+
+}
