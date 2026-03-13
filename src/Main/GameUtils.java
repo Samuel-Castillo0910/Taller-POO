90cd9960fba8a23e559f68ce442bfbf1cc79e5e8
Samuel Castillo <samuelcastillo0508@gmail.com>
2026-03-13 09:33:01 -0500

    GameUtils Listo para usar

--- src/Main/GameUtils.java
+++ src/Main/GameUtils.java
@@ -2,6 +2,14 @@ package Main;
 
 public final class GameUtils {
 
+public static Object[] resizeArray(Object[] array, int newSize){
+    Object[] newArray = new Object[newSize];
 
+    for(int i=0; i < array.length; i++){
+        newArray[i] = array[i];
+    }
+
+    return newArray;
+}
 
 }
