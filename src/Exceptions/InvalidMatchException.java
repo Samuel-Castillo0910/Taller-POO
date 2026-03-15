418a676f1023c950dee5a3a4e4914015f13c6f28
Samuel Castillo <samuelcastillo0508@gmail.com>
2026-03-15 16:00:07 -0500

    Change InvalidMatchException to extend Exception

--- src/Exceptions/InvalidMatchException.java
+++ src/Exceptions/InvalidMatchException.java
@@ -1,6 +1,6 @@
 package Exceptions;
 
-public class InvalidMatchException extends RuntimeException {
+public class InvalidMatchException extends Exception {
     public InvalidMatchException(String message) {
         super(message);
     }
